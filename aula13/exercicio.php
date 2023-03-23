1197f6d0d469d0b969a2ec7ed5166fd190b422f4
Ana Clara BrandÃ£o Garcia <02257701640@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-23 11:26:06 -0300

    Aula get

---
+++ aula13/exercicio.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_GET['numero']) && isset($_GET['n2'])){
+    $r = $_GET['numero'] + $_GET['n2'];
+    echo $r;
+}else{
+    echo "Você precisa informar os valores para somar";
+
+    //else "Você precisa informar os valores para somar";
+    //faz um redirecionamento
+    header("Location: form1.html");
+}
+
+?>
---
+++ aula14/exercicio.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['numero']) && isset($_POST['n2'])){
+    $r = $_POST['numero'] + $_POST['n2'];
+    echo $r;
+}else{
+    echo "Você precisa informar os valores para somar";
+
+    //else "Você precisa informar os valores para somar";
+    //faz um redirecionamento
+    header("Location: form1.html");
+}
+
+?>
