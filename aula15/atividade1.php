dfad65f7d8107c3a44707c955d6a8468667cdd7f
Ana Clara BrandÃ£o Garcia <02257701640@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-27 08:57:48 -0300

    Aula15

---
+++ aula15/atividade1.php
@@ -0,0 +1,54 @@
+<?php
+
+$cores=[];
+$cores []= "yellow";
+$cores []= "green";
+$cores []= "blue";
+$cores []= "pink";
+$cores []= "red";
+$cores []= "purple";
+$cores []= "orange";
+$cores []= "gray";
+$cores []= "white";
+
+ 
+
+if(isset($_GET['cor'])){
+    $cor = $_GET['cor'];
+  }else{
+    $cor = 'white';
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <style>
+   body {
+            background-color: <?php echo $cor ?>;
+        }
+    </style>
+</head>
+<body>
+
+ 
+
+<?php
+foreach($cores as $v){
+  echo "<a href='atividade1.php?cor=$v'>$v</a>";
+}
+
+?>
+
+
+
+
+
+
+</body>
+</html>
