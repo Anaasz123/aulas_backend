b456ed094fb3099ca04e25e2a88635216fcca109
Ana Clara BrandÃ£o Garcia <02257701640@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-04-27 10:36:18 -0300

    aula 18

---
+++ aula18/conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = "127.0.0.1";
+$port = "3306";
+$user = "root";
+$passaword = "";
+$database = "db_catalogo_3infoa";
+
+//faz a conexao do PHP com o MySQL
+$conexao = new mysqli($host, 
+                      $user,
+                      $passaword,
+                      $database,
+                      $port);
+
+//Verifica se houve um erro ao realizar a conexao
+if($conexao->connect_error){
+    echo "Foi encontrado o erro : " .
+                        $conexao->connect_error;
+
+  die();
+
+
+}
